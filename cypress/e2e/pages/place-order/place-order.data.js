756821ccfb1b7819938a812f57b87c99bcc0044e
NataliaBEspinosa <nataliaespinosa2112@gmail.com>
2025-09-23 15:41:54 -0300

    Creación de la página place order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,13 @@
+export class PlacerOrderData{
+    static get testDate(){
+        return{
+            name: 'test name',
+            country: 'Argentina',
+            city: 'Mar del Plata',
+            creditCardNumber: '4509953566233704',
+            month: '12',
+            year: 2024,
+        }
+    }
+    
+}
---
+++ cypress/e2e/pages/place-order/place-order.methods.js
@@ -0,0 +1,31 @@
+import { PlacerOrderElements } from "./place-order.elements";
+
+export class PlacerOrderMethods{
+    static insertName (name){
+        PlacerOrderElements.texboxes.name.invoke('val', name);
+    }
+     static insertCountry (country){
+        PlacerOrderElements.texboxes.country.invoke('val', country);
+    }
+    static insertCity (city){
+        PlacerOrderElements.texboxes.city.invoke('val', city);
+    }
+    static insertCreditCard(creditCardNumber){
+        PlacerOrderElements.texboxes.creditCard.invoke('val',creditCardNumber);
+    }
+    static insertMonth(month){
+        PlacerOrderElements.texboxes.month.invoke('val',month);
+    }
+    static insertYear(year){
+        PlacerOrderElements.texboxes.year.invoke('val',year);
+    }
+
+    static clickOnCloseButton(){
+        PlacerOrderElements.buttons.close.click()
+    }
+
+    static clickOnPuchaseButton(){
+        PlacerOrderElements.buttons.purchase.click()
+    }
+
+}
